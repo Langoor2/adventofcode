b526f53cfc93614468de4af3311743aebd666efb
Langoor2 <github@nielsdv.eu>
2022-12-06 08:56:10 +0100

    add day 6 in python

---
+++ 2022/day6/day6.py
@@ -0,0 +1,42 @@
+with open("input") as file:
+     lines = [item.strip() for item in file.readlines()]
+
+pos=0
+packets=[]
+firstpos = 0
+firstset = False
+
+for line in lines:
+     for char in line:
+          pos += 1
+          if len(packets) < 4:
+               packets.append(char)
+          else:
+               if (len(packets)==len(set(packets))) == True:
+                    if firstset == False:
+                         firstset = True
+                         firstpos = pos
+               packets.pop(0)
+               packets.append(char)
+print (firstpos-1)
+
+
+pos=0
+packets=[]
+firstpos = 0
+firstset = False
+
+for line in lines:
+     for char in line:
+          pos += 1
+          if len(packets) < 13:
+               packets.append(char)
+          else:
+               if (len(packets)==len(set(packets))) == True:
+                    if firstset == False:
+                         firstset = True
+                         firstpos = pos
+               packets.pop(0)
+               packets.append(char)
+print (firstpos)
+
